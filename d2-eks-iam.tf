a78e5e693c291b4956d608a13245bc8ef9f8a503
Fatma Erdogan <66177009+fatma-erdogan@users.noreply.github.com>
2022-09-12 14:09:30 -0700

    Create d2-eks-iam.tf

---
+++ d2-eks-iam.tf
@@ -0,0 +1,76 @@
+# Create IAM Role
+resource "aws_iam_role" "eks_master_role" {
+  name = "${var.project}-${var.env}-controlplane-iam-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Associate IAM Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_master_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_master_role.name
+}
+resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_master_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_master_role.name
+}
+resource "aws_iam_role_policy_attachment" "eks-AmazonAWSAppRunnerPolicyForECRAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+  role       = aws_iam_role.eks_master_role.name
+}
+resource "aws_iam_role_policy_attachment" "eks-ec2-describe-policy" {
+  policy_arn = aws_iam_policy.ec2-describe-policy.arn
+  role       = aws_iam_role.eks_master_role.name
+}
+
+
+resource "aws_iam_policy" "ec2-describe-policy" {
+  name        = "ec2-describe-policy-2"
+ 
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeAddresses",
+                "ec2:DescribeInternetGateways"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+})
+}
+
+/*
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_master_role.name
+}
+*/
